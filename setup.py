58449d5d22573837f67415b1d03a27dbaeafb1e6
Beki Imanji <imanaliev@akhter.studio>
2022-11-19 14:26:33 +0600

    new changes

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import (
+    find_packages,
+    setup,
+)
+
+VERSION = "0.1.0"
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name='rpc',
+    author="Imanji Beki",
+    author_email="imanjibeki@gmail.com",
+    packages=find_packages(),
+    url="",
+    download_url="https://github.com/kshcherban/acme-nginx/tarball/v{0}".format(
+        VERSION
+    ),
+    license="MIT",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "aio-pika==8.2.4",
+        "fastapi==0.85.0",
+    ],
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+)
