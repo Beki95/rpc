21a42621b12942bb1d6f4471d5d652ae5f6dafac
Beki Imanji <imanaliev@akhter.studio>
2023-01-01 15:25:30 +0600

    added protocols

--- remote_procedure/rabbitmq/protocols.py
+++ remote_procedure/rabbitmq/protocols.py
@@ -1,12 +1,15 @@
 import abc
-from typing import Any
+from typing import (
+    Any,
+    Callable,
+)
 
 from aio_pika.abc import (
     AbstractIncomingMessage,
-    AbstractRobustChannel,
-    AbstractRobustConnection,
+    TimeoutType,
 )
 from aio_pika.patterns import RPC
+from pika.adapters.blocking_connection import BlockingChannel
 
 from remote_procedure.rabbitmq.type import (
     JsonRPC,
@@ -14,22 +17,10 @@ from remote_procedure.rabbitmq.type import (
 )
 
 
-class RPCClientProtocol(abc.ABC):
+class RPCAsyncClientProtocol(abc.ABC):
 
     @abc.abstractmethod
     def __init__(self, url: str, rpc: UnionRpc = JsonRPC):  # noqa
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def set_event_loop(self, loop):
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    async def connection_factory(self, **kwargs) -> AbstractRobustConnection:
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    async def get_channel(self) -> AbstractRobustChannel:
         raise NotImplementedError
 
     @abc.abstractclassmethod
@@ -37,28 +28,84 @@ class RPCClientProtocol(abc.ABC):
         raise NotImplementedError
 
     @abc.abstractmethod
-    async def publish(self, body: Any, queue_name):
+    async def rpc_call(self, body: Any, queue_name, timeout: TimeoutType):
         raise NotImplementedError
 
 
-class RPCServerProtocol(abc.ABC):
+class RPCAsyncServerProtocol(abc.ABC):
 
-    @abc.abstractmethod
-    def __init__(self, url, rpc: UnionRpc = JsonRPC):  # noqa
-        raise NotImplementedError
-
-    @abc.abstractmethod
     def set_event_loop(self, loop):
-        raise NotImplementedError
+        self.loop = loop  # noqa
 
     @abc.abstractmethod
     def include_router(self, router, *, prefix: str = '') -> None:
         raise NotImplementedError
 
     @abc.abstractmethod
-    async def connection(self) -> AbstractRobustConnection:
+    async def launch(self) -> RPC:
+        raise NotImplementedError
+
+
+class RPCSyncClientProtocol(abc.ABC):
+    @abc.abstractmethod
+    def on_response(self, ch: BlockingChannel, method, props, body):
         raise NotImplementedError
 
     @abc.abstractmethod
-    async def execute(self) -> RPC:
+    def consume(self, queue, channel: BlockingChannel):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def rpc_call(self, routing_key, body, timout: TimeoutType):
+        raise NotImplementedError
+
+
+class RPCSyncServerProtocol(abc.ABC):
+    @abc.abstractmethod
+    def include_router(self, router, *, prefix: str = '') -> None:
+        raise NotImplementedError
+
+    @abc.abstractclassmethod
+    def call_func(cls, func: Callable[..., Any], payload):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def register(
+            self,
+            *,
+            method_name,
+            func: Callable[..., Any],
+            **kwargs,
+    ):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def on_call_message(self, ch, method, props, body):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def launch(self) -> None:
+        raise NotImplementedError
+
+
+class PoolProtocol(abc.ABC):
+
+    @abc.abstractmethod
+    def filling_the_pool(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def acquire(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def realise(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def __enter__(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def __exit__(self, exc_type, exc_val, exc_tb):
         raise NotImplementedError
