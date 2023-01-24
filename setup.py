a59321c7aba874b55dde775ea834136372b1e654
Beki Imanji <imanaliev@akhter.studio>
2023-01-24 10:27:56 +0600

    bug fix version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import (
     setup,
 )
 
-VERSION = "1.2.5"
+VERSION = "1.2.6"
 NAME: str = 'remote_procedure'
 AUTHOR: str = 'Imanji Beki'  # noqa
 AUTHOR_EMAIL: str = 'imanjibeki@gmail.com'
