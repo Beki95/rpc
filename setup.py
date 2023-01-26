bc35df90cc29882391eb0627016290931d090618
Beki Imanji <imanaliev@akhter.studio>
2023-01-26 13:58:36 +0600

    fix heartbeat

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import (
     setup,
 )
 
-VERSION = "1.2.7"
+VERSION = "1.2.8"
 NAME: str = 'remote_procedure'
 AUTHOR: str = 'Imanji Beki'  # noqa
 AUTHOR_EMAIL: str = 'imanjibeki@gmail.com'
