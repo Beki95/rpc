8f9b83a8c5ad6ddd6c030a9b8dc03b249c6ebd4c
Beki Imanji <imanaliev@akhter.studio>
2023-01-27 16:39:37 +0600

    attempt to fix a connection bug

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import (
     setup,
 )
 
-VERSION = "1.2.8"
+VERSION = "1.2.9"
 NAME: str = 'remote_procedure'
 AUTHOR: str = 'Imanji Beki'  # noqa
 AUTHOR_EMAIL: str = 'imanjibeki@gmail.com'
